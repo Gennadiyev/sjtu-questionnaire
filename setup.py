94226b0aa72fa6a72b9e92163ea159f2a8453046
Kunologist <2014709936@qq.com>
2022-10-24 12:00:07 +0800

    [feat] Migrate to loguru

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from distutils.core import setup
 
 setup(
     name='sjtu-questionnaire',
-    version='0.1.0',
+    version='0.2.0',
     description='A Python binding to SJTU Questionnaire API (https://wj.sjtu.edu.cn/)',
     author='Kunologist',
     author_email='kunologist@foxmail.com',
-    url='https://github.com/Gennadiyev/sjtuq',
+    url='https://github.com/Gennadiyev/sjtu-questionnaire',
     packages=['sjtuq'],
-    install_requires=['requests'],
+    install_requires=['requests', 'loguru'],
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
     license='MIT',
@@ -17,6 +17,12 @@ setup(
         "Operating System :: OS Independent",
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python :: 3'
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+        'Topic :: Software Development :: Libraries :: Python Modules :: API Bindings',
     ]
 )
