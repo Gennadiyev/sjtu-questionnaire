4deb52a8989e069643aab9e431e9c4f900460320
Kunologist <2014709936@qq.com>
2022-10-24 12:00:00 +0800

    [feat] Migrate to loguru

--- sjtuq/__init__.py
+++ sjtuq/__init__.py
@@ -3,14 +3,17 @@
 @author: Kunologist
 '''
 
+__version__ = '0.2.0'
+__author__ = 'Kunologist'
+
 from copy import deepcopy
-import multiprocessing
-from multiprocessing import freeze_support
+import multiprocessing as mp
+from typing import Union
 import requests
-import logging
+from loguru import Logger, logger
 import json
 
-freeze_support()
+mp.freeze_support()
 
 class SJTUQuestionnaire():
     '''An SJTU Questionnaire class for Object-Oriented Programming
@@ -26,7 +29,7 @@ class SJTUQuestionnaire():
     '''
     sjtu_questionnaire_api_format_url = "https://wj.sjtu.edu.cn/api/v1/public/export/{key}/json"
 
-    def __init__(self, url_or_key:str, logger:logging.Logger=None):
+    def __init__(self, url_or_key:str, logger: Logger=None):
         '''Initialize an SJTU Questionnaire object
 
         @param url_or_key: The URL of the SJTU Questionnaire API or the key of the SJTU Questionnaire API
@@ -47,21 +50,19 @@ class SJTUQuestionnaire():
             raise ValueError("Invalid url_or_key: {}".format(url_or_key))
         else:
             raise TypeError("url_or_key must be a string, but got {}".format(type(url_or_key)))
-        if isinstance(logger, logging.Logger):
+        if isinstance(logger, Logger):
             self.logger = logger
             self.has_logger = True
         elif logger is None:
             self.logger = logger
             self.has_logger = logger is not None
         else:
-            raise TypeError("logger must be a logging.Logger object, but got {}".format(type(logger)))
+            raise TypeError("logger must be a loguru.Logger object, but got {}".format(type(logger)))
 
-    def __log(self, msg:str, level:int=logging.DEBUG):
+    def __log(self, msg:str, level: Union[int, str]="DEBUG"):
         if self.has_logger:
             self.logger.log(level, msg)
-        # elif level in (logging.DEBUG, logging.INFO, logging.WARNING, logging.ERROR, logging.CRITICAL):
-        #     print(msg)
-            
+
     def get_data(self, params:dict=None, sort:str="asc") -> dict:
         '''Send a request to SJTU Questionnaire API. This is just a wrapper for C{requests.get}. It is the developer's responsibility to check the validity, usually whether C{ret["success"]} is C{True}.
 
@@ -79,13 +80,13 @@ class SJTUQuestionnaire():
         '''
         # Type and value check
         if params is None:
-            self.__log("params not specified, defaults to an empty dict", level=logging.DEBUG)
+            self.__log("params not specified, defaults to an empty dict", level="DEBUG")
             params = {}
         elif not isinstance(params, dict):
-            self.__log("params must be a dict, but got {}".format(type(params)), logging.ERROR)
+            self.__log("params must be a dict, but got {}".format(type(params)), level="ERROR")
             raise ValueError("params must be a dict, but got {}".format(type(params)))
         if sort not in ("asc", "desc"):
-            self.__log("sort must be either \"asc\" or \"desc\", but got {}".format(sort), logging.ERROR)
+            self.__log("sort must be either \"asc\" or \"desc\", but got {}".format(sort), level="ERROR")
             raise ValueError("Invalid sort, must be \"asc\" or \"desc\", but got \"{}\"".format(sort))
         # Check params key
         # TODO
@@ -100,11 +101,11 @@ class SJTUQuestionnaire():
         response = requests.get(self.url, params=real_params)
         self.__log("Sending GET request to {}".format(response.url))
         if response.status_code != 200:
-            self.__log("Error: Status code {} in get_data".format(response.status_code), level=logging.ERROR)
-            self.__log("       Response: {}".format(response.text), level=logging.ERROR)
+            self.__log("Error: Status code {} in get_data".format(response.status_code), level="ERROR")
+            self.__log("       Response: {}".format(response.text), level="ERROR")
         return response.json()
 
-    def get_all_data(self, params:dict=None, sort:str="asc") -> list:
+    def get_all_data(self, params:dict=None, sort:str="asc", pool_size: int=None) -> list:
         '''Get all the data of the questionnaire. This will send multiple requests to the server, and the data will be merged into a list of user answers.
 
         @param params: The parameters of the SJTU Questionnaire API, defaults to None (treated as an empty dict).
@@ -113,6 +114,7 @@ class SJTUQuestionnaire():
         Some values will be overridden, such as C{pageSize} and C{current}.
         
         @param sort: The sort of the SJTU Questionnaire API, defaults to "asc". It can be "asc" or "desc".
+        @param pool_size: The size of the thread pool, defaults to None (treated as C{mp.cpu_count()}). Set to 1 to disable multithreading.
         @return: All the data of the SJTU Questionnaire API
         @rtype: list
 
@@ -123,20 +125,20 @@ class SJTUQuestionnaire():
         '''
         # Type and value check
         if params is None:
-            self.__log("params not specified, defaults to an empty dict", level=logging.DEBUG)
+            self.__log("params not specified, defaults to an empty dict", level="DEBUG")
             params = {}
         elif not isinstance(params, dict):
-            self.__log("params must be a dict, but got {}".format(type(params)), level=logging.ERROR)
+            self.__log("params must be a dict, but got {}".format(type(params)), level="ERROR")
             raise ValueError("params must be a dict, but got {}".format(type(params)))
         if sort not in ("asc", "desc"):
-            self.__log("Invalid sort, must be \"asc\" or \"desc\", but got \"{}\"".format(sort), level=logging.ERROR)
+            self.__log("Invalid sort, must be \"asc\" or \"desc\", but got \"{}\"".format(sort), level="ERROR")
             raise ValueError("Invalid sort, must be \"asc\" or \"desc\", but got \"{}\"".format(sort))
         
         # Check params key
         if "pageSize" in params:
-            self.__log("pageSize will be overridden", level=logging.WARNING)
+            self.__log("pageSize will be overridden", level="WARNING")
         if "current" in params:
-            self.__log("current will be overridden", level=logging.WARNING)
+            self.__log("current will be overridden", level="WARNING")
         
         # First check the total number of data
         params_copy = deepcopy(params)
@@ -144,7 +146,7 @@ class SJTUQuestionnaire():
         params_copy["current"] = 1 
         response = self.get_data(params_copy, sort=sort)
         if not ("success" in response and response["success"]):
-            self.__log("Error: Failed to get the total number of data", level=logging.ERROR)
+            self.__log("Error: Failed to get the total number of data", level="ERROR")
             return []
         total = response["data"]["total"]
         self.__log("Total number of rows: {}".format(total))
@@ -152,8 +154,8 @@ class SJTUQuestionnaire():
         # Use multiprocessing to speed up the process
         params_copy["pageSize"] = 100
         responses = []
-        pool = multiprocessing.Pool(processes=multiprocessing.cpu_count())
-        self.__log("Multiprocessing pool size: {}".format(multiprocessing.cpu_count()))
+        pool = mp.Pool(processes=mp.cpu_count())
+        self.__log("Multiprocessing pool size: {}".format(mp.cpu_count()))
         # Send requests
         for i in range(1, int(total/100)+2):
             params_copy["current"] = i
